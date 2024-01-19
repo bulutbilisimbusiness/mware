0f6415bc95a1b7e31d93c5efab5ad140dac7222e
erhan bulut <90501327+bulutbilisimbusiness@users.noreply.github.com>
2024-01-19 21:55:50 +0300

    permissions

--- personnelAPI/src/routes/personnel.router.js
+++ personnelAPI/src/routes/personnel.router.js
@@ -2,17 +2,17 @@
 
 const router = require('express').Router()
 const personnel = require('../controllers/personnel.controller')
-
+const permissions=require('../middlewares/permissions')
 router.post('/login',personnel.login)
 router.all('/logout',personnel.logout)
 
 router.route('/')
-    .get(personnel.list)
-    .post(personnel.create)
+    .get(permissions.isAdmin,personnel.list)
+    .post(permissions.isAdmin,personnel.create)
 
 router.route('/:id')
-    .get(personnel.read)
-    .put(personnel.update)
-    .patch(personnel.update)
-    .delete(personnel.delete)
+    .get(permissions.isAdminOrOwner,personnel.read)
+    .put(permissions.isAdminOrOwner,personnel.update)
+    .patch(permissions.isAdminOrOwner,personnel.update)
+    .delete(permissions.isAdmin,personnel.delete)
 module.exports = router
