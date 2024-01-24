41130c589f2f31b01c71e9e45125996f25a3ceca
erhan bulut <90501327+bulutbilisimbusiness@users.noreply.github.com>
2024-01-24 22:32:05 +0300

    user

--- pizzaAPI/index.js
+++ pizzaAPI/index.js
@@ -1,6 +1,4 @@
 "use strict"
-
-
 const express = require('express')
 const app = express()
 
@@ -12,6 +10,20 @@ require('express-async-errors')
 
 const { dbConnection } = require('./src/configs/dbConnection')
 dbConnection()
+
+app.use(express.json())
+app.use(require('./src/middlewares/logger'))
+
+app.use(require('./src/middlewares/findSearchSortPage'))
+
+app.all('/',(req,res)=>{
+    res.send({
+        error:false,
+        message:'Welcome to PIZZA API',
+        isLogin:req.isLogin,
+        user:req.user
+    })
+})
 
 
 app.use(require('./src/middlewares/errorHandler'))
