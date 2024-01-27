d7d47bba1fab7f81aff187bafacea0f1489e84a3
erhan bulut <90501327+bulutbilisimbusiness@users.noreply.github.com>
2024-01-27 22:31:26 +0300

    flightapi

---
+++ flightAPI/src/middlewares/authentication.js
@@ -0,0 +1,15 @@
+"use strict"
+
+// $ npm i jsonwebtoken
+// app.use(authentication):
+
+const jwt = require('jsonwebtoken')
+
+module.exports = (req, res, next) => {
+
+    const auth = req.headers?.authorization || null
+    const accessToken = auth ? auth.split(' ')[1] : null
+
+    jwt.verify(accessToken, process.env.ACCESS_KEY, (err, userData) => req.user = userData)
+    next()
+}
