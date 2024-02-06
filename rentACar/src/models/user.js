003b4c89760e566b2d701cc89a30440e0625200f
erhan bulut <90501327+bulutbilisimbusiness@users.noreply.github.com>
2024-02-06 12:24:02 +0300

    gitnore

--- rentACar/src/models/user.js
+++ rentACar/src/models/user.js
@@ -1,6 +1,6 @@
-"use strict"
+"use strict";
 
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- *
 {
     "username": "admin",
@@ -22,3 +22,66 @@ const { mongoose } = require('../configs/dbConnection')
 }
 /* ------------------------------------------------------- */
 // User Model:
+const UserSchema = new mongoose.Schema(
+	{
+		username: {
+			type: String,
+			trim: true,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			trim: true,
+			required: true,
+		},
+		email: {
+			type: String,
+			trim: true,
+			required: true,
+			unique: true,
+		},
+		firstName: {
+			type: String,
+			trim: true,
+			required: true,
+		},
+		lastName: {
+			type: String,
+			trim: true,
+			required: true,
+		},
+		isActive: {
+			type: Boolean,
+			default: true,
+		},
+		isAdmin: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{
+		collection: "users",
+		timestamps: true,
+	}
+);
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+UserSchema.pre("save", function (next) {
+	const isEmailValidated = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
+		this.email
+	);
+	if (isEmailValidated) {
+		const isPasswordValidated = Regexp(
+			"^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&].{8,}$"
+		).test(this.password);
+		if (isPasswordValidated) {
+			this.password = passwordEncrypt(this.password);
+			next();
+		} else {
+			next(new Error("Password not validated."));
+		}
+	} else {
+		next(new Error("Email not validated."));
+	}
+});
+module.exports=mongoose.model('User',UserSchema)
